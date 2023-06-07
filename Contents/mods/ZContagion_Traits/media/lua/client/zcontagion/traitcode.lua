40497ca92662b86636e9d96a69829ed7c601c88e
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-06-07 07:18:21 +0100

    fix point exploit by changing price

--- Contents/mods/ZContagion_Traits/media/lua/client/zcontagion/traitcode.lua
+++ Contents/mods/ZContagion_Traits/media/lua/client/zcontagion/traitcode.lua
@@ -1,6 +1,7 @@
+local previousPrice = 0
+
 local old_setSandboxVars = SandboxOptionsScreen.setSandboxVars
----@param self CharacterCreationProfession
----@param visible boolean
+---@param self SandboxOptionsScreen
 SandboxOptionsScreen.setSandboxVars = function(self)
     old_setSandboxVars(self)
     local trait = TraitFactory.getTrait("Carrier")
@@ -12,18 +13,19 @@ SandboxOptionsScreen.setSandboxVars = function(self)
             or ccp.listboxBadTrait:removeItem(label)
             or ccp.listboxTraitSelected:removeItem(label)
 
-    if not olditem then return end
+    if olditem then -- readd it
+        local newItem
+        if trait:getCost() > 0 then
+            newItem = ccp.listboxTrait:addItem(label, trait)
+        else
+            newItem = ccp.listboxBadTrait:addItem(label, trait)
+        end
+        newItem.tooltip = trait:getDescription()
+        ccp.pointToSpend = ccp.pointToSpend + previousPrice
 
-    --readd it
-    local newItem
-    if trait:getCost() > 0 then
-        newItem = ccp.listboxTrait:addItem(label, trait)
-    else
-        newItem = ccp.listboxBadTrait:addItem(label, trait)
+        CharacterCreationMain.sort(ccp.listboxTrait.items)
+        CharacterCreationMain.invertSort(ccp.listboxBadTrait.items)
+        CharacterCreationMain.sort(ccp.listboxTraitSelected.items)
     end
-    newItem.tooltip = trait:getDescription()
-
-    CharacterCreationMain.sort(ccp.listboxTrait.items)
-    CharacterCreationMain.invertSort(ccp.listboxBadTrait.items)
-    CharacterCreationMain.sort(ccp.listboxTraitSelected.items)
+    previousPrice = trait:getCost()
 end
