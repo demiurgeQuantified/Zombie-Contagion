b1dffa92a4dc3b4fa3032b8ea7de6974c1ef1483
danielewartwelch <danielewartwelch@gmail.com>
2023-06-14 16:31:48 -0400

    fixing pointToSpend bug and only removing carrier trait if purchased

--- Contents/mods/ZContagion_Traits/media/lua/client/zcontagion/traitcode.lua
+++ Contents/mods/ZContagion_Traits/media/lua/client/zcontagion/traitcode.lua
@@ -7,13 +7,27 @@ SandboxOptionsScreen.setSandboxVars = function(self)
     local trait = TraitFactory.getTrait("Carrier")
     local ccp = MainScreen.instance.charCreationProfession
 
+    
     --cleanup old trait
     local label = trait:getLabel()
-    local olditem = ccp.listboxTrait:removeItem(label)
+    
+    --determine if trait is purchased before clearing ccp.listboxTraitSelected
+    local traitIsPurchased = false
+    for _,v in ipairs(ccp.listboxTraitSelected.items) do
+        if v.item:getLabel() == label then 
+            traitIsPurchased = true 
+        end
+    end
+
+    --only remove traits if the price has actually changed
+    local oldItem 
+    if(previousPrice ~= trait:getCost()) then
+        oldItem = ccp.listboxTrait:removeItem(label)
             or ccp.listboxBadTrait:removeItem(label)
             or ccp.listboxTraitSelected:removeItem(label)
+    end
 
-    if olditem then -- readd it
+    if oldItem then -- readd it
         local newItem
         if trait:getCost() > 0 then
             newItem = ccp.listboxTrait:addItem(label, trait)
@@ -21,8 +35,12 @@ SandboxOptionsScreen.setSandboxVars = function(self)
             newItem = ccp.listboxBadTrait:addItem(label, trait)
         end
         newItem.tooltip = trait:getDescription()
-        ccp.pointToSpend = ccp.pointToSpend + previousPrice
-
+        
+        --adjust available points only if traitIsPurchased
+        if traitIsPurchased then
+            ccp.pointToSpend = ccp.pointToSpend + previousPrice
+        end
+        
         CharacterCreationMain.sort(ccp.listboxTrait.items)
         CharacterCreationMain.invertSort(ccp.listboxBadTrait.items)
         CharacterCreationMain.sort(ccp.listboxTraitSelected.items)
