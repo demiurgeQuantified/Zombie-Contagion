f8a1cabfbef6c24a2e0a9cae24dbe206f6746c29
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-06-16 17:46:55 +0100

    fix trait showing in wrong menu in mp + optimise

--- Contents/mods/ZContagion_Traits/media/lua/client/zcontagion/traitcode.lua
+++ Contents/mods/ZContagion_Traits/media/lua/client/zcontagion/traitcode.lua
@@ -1,49 +1,58 @@
 local previousPrice = 0
 
+-- TODO: it shows in the wrong list if you change the price and then go back to a preset difficulty (who cares, low prio)
+-- TODO: we don't really need to remove the trait from the selection if it now grants more points
+-- also consider not removing it the player can still afford it in general
+local function updateTraitPrice()
+    local trait = TraitFactory.getTrait("Carrier")
+    local newCost = trait:getCost()
+
+    --only remove traits if the price has actually changed
+    if previousPrice == newCost then return end
+
+    local ccp = MainScreen.instance.charCreationProfession
+    local label = trait:getLabel()
+
+    --determine if trait is purchased before clearing ccp.listboxTraitSelected
+    local traitIsPurchased = false
+    local selectedItems = ccp.listboxTraitSelected.items
+    for i=1, #selectedItems do
+        if selectedItems[i].item == trait then
+            traitIsPurchased = true
+            break
+        end
+    end
+
+    local oldItem = ccp.listboxTrait:removeItem(label)
+            or ccp.listboxBadTrait:removeItem(label)
+            or traitIsPurchased and ccp.listboxTraitSelected:removeItem(label)
+    if not oldItem then return end
+
+    -- readd it
+    local newItem
+    if newCost > 0 then
+        newItem = ccp.listboxTrait:addItem(label, trait)
+    else
+        newItem = ccp.listboxBadTrait:addItem(label, trait)
+    end
+    newItem.tooltip = trait:getDescription()
+
+    --adjust available points only if traitIsPurchased
+    if traitIsPurchased then
+        ccp.pointToSpend = ccp.pointToSpend + previousPrice
+    end
+
+    CharacterCreationMain.sort(ccp.listboxTrait.items)
+    CharacterCreationMain.invertSort(ccp.listboxBadTrait.items)
+    CharacterCreationMain.sort(ccp.listboxTraitSelected.items)
+    previousPrice = newCost
+end
+
 local old_setSandboxVars = SandboxOptionsScreen.setSandboxVars
 ---@param self SandboxOptionsScreen
 SandboxOptionsScreen.setSandboxVars = function(self)
     old_setSandboxVars(self)
-    local trait = TraitFactory.getTrait("Carrier")
-    local ccp = MainScreen.instance.charCreationProfession
-
-    
-    --cleanup old trait
-    local label = trait:getLabel()
-    
-    --determine if trait is purchased before clearing ccp.listboxTraitSelected
-    local traitIsPurchased = false
-    for _,v in ipairs(ccp.listboxTraitSelected.items) do
-        if v.item:getLabel() == label then 
-            traitIsPurchased = true 
-        end
-    end
-
-    --only remove traits if the price has actually changed
-    local oldItem 
-    if(previousPrice ~= trait:getCost()) then
-        oldItem = ccp.listboxTrait:removeItem(label)
-            or ccp.listboxBadTrait:removeItem(label)
-            or ccp.listboxTraitSelected:removeItem(label)
-    end
-
-    if oldItem then -- readd it
-        local newItem
-        if trait:getCost() > 0 then
-            newItem = ccp.listboxTrait:addItem(label, trait)
-        else
-            newItem = ccp.listboxBadTrait:addItem(label, trait)
-        end
-        newItem.tooltip = trait:getDescription()
-        
-        --adjust available points only if traitIsPurchased
-        if traitIsPurchased then
-            ccp.pointToSpend = ccp.pointToSpend + previousPrice
-        end
-        
-        CharacterCreationMain.sort(ccp.listboxTrait.items)
-        CharacterCreationMain.invertSort(ccp.listboxBadTrait.items)
-        CharacterCreationMain.sort(ccp.listboxTraitSelected.items)
-    end
-    previousPrice = trait:getCost()
+    updateTraitPrice()
 end
+
+Events.OnConnected.Add(updateTraitPrice)
