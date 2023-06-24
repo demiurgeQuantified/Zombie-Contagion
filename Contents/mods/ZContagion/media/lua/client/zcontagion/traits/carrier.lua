c18decfe7b2a2e4c99a5fb4f077f8b06698d28ed
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-06-24 19:03:46 +0100

    add random chance to gain the carrier trait on character creation

--- Contents/mods/ZContagion/media/lua/client/zcontagion/traits/carrier.lua
+++ Contents/mods/ZContagion/media/lua/client/zcontagion/traits/carrier.lua
@@ -1,4 +1,6 @@
 local Carrier = {}
+Carrier.wantRandomCarriers = true
+Carrier.carrierChance = 5
 
 ---@param player IsoPlayer
 Carrier.OnPlayerDeath = function(player)
@@ -7,5 +9,16 @@ Carrier.OnPlayerDeath = function(player)
 end
 
 Events.OnPlayerDeath.Add(Carrier.OnPlayerDeath)
+
+---@param player IsoPlayer
+---@param square IsoGridSquare
+Carrier.OnNewGame = function(player, square)
+    if not Carrier.wantRandomCarriers then return end -- TODO: add sandbox option
+    if ZombRand(100) + 1 <= Carrier.carrierChance then
+        player:getTraits():add("Carrier")
+        -- TODO: some way to make this hidden (removing the trait icon might be enough?)
+    end
+end
+Events.OnNewGame.Add(Carrier.OnNewGame)
 
 return Carrier
