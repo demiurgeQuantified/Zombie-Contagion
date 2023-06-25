e1b451ce069d8d6c8fb51bc7be123410d0627c4d
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-06-25 05:19:31 +0100

    add random carrier sandbox option

--- Contents/mods/ZContagion/media/lua/client/zcontagion/traits/carrier.lua
+++ Contents/mods/ZContagion/media/lua/client/zcontagion/traits/carrier.lua
@@ -1,6 +1,22 @@
 local Carrier = {}
-Carrier.wantRandomCarriers = true
-Carrier.carrierChance = 5
+
+Carrier.carrierChances = {
+    [1] = 0,
+    [2] = 0,
+    [3] = 15,
+    [4] = 30,
+    [5] = 50
+}
+
+Carrier.traitModifiers = {
+    ProneToIllness = -0.7,
+    Resilient = 0.7,
+    Lucky = 0.1,
+    Unlucky = -0.1,
+    Outdoorsman = 0.1,
+    Asthmatic = -0.2,
+    Smoker = -0.2
+}
 
 ---@param player IsoPlayer
 Carrier.OnPlayerDeath = function(player)
@@ -11,10 +27,25 @@ end
 Events.OnPlayerDeath.Add(Carrier.OnPlayerDeath)
 
 ---@param player IsoPlayer
+---@return int
+Carrier.getCarrierChance = function(player)
+    local chance = Carrier.carrierChances[SandboxVars.ZContagion.CarrierChance]
+    if chance == 0 then return 0 end
+
+    local traits = player:getTraits()
+    for i = 0, traits:size()-1 do
+        local traitMult = Carrier.traitModifiers[traits:get(i)]
+        if traitMult then
+            chance = chance * (1 + traitMult)
+        end
+    end
+    return math.floor(chance)
+end
+
+---@param player IsoPlayer
 ---@param square IsoGridSquare
 Carrier.OnNewGame = function(player, square)
-    if not Carrier.wantRandomCarriers then return end -- TODO: add sandbox option
-    if ZombRand(100) + 1 <= Carrier.carrierChance then
+    if ZombRand(100) + 1 <= Carrier.getCarrierChance(player) then
         player:getTraits():add("Carrier")
         -- TODO: some way to make this hidden (removing the trait icon might be enough?)
     end
